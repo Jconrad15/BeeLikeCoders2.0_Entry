31695c5e404d2a7bdc3da842b1013ba81a7febc1
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2023-04-15 14:56:28 -0700

    Get background audio

---
+++ Mindful_Hiker/Assets/Scripts/Audio/BackgroundSoundController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSoundController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip[] bgMusic;
+
+    private int index;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        index = 0;
+        Play();
+    }
+
+    private void Update()
+    {
+        if (audioSource.isPlaying == false)
+        {
+            index++;
+            if (index >= bgMusic.Length)
+            {
+                index = 0;
+            }
+
+            Play();
+        }
+    }
+
+    private void Play()
+    {
+        audioSource.clip = bgMusic[index];
+        audioSource.Play();
+    }
+}
